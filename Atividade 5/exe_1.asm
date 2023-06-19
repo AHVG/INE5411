09005393b7573bdd24e8a765774e0076ba7da3b6
Augusto <augusto.h.v.guerner@gmail.com>
2023-06-19 12:03:44 -0300

    Arrumando mais uma vez os registradores

--- Atividade 5/exe_1.asm
+++ Atividade 5/exe_1.asm
@@ -7,11 +7,11 @@
 
 	# alocar A e B
 	
-	mul $t1, $s0, $s0 # t1 = MAX*MAX
-	mul $t1, $t1, 4 # t1 = MAX*MAX*4 (tamanho de bytes de cada matriz)
+	mul $t0, $s0, $s0 # t0 = MAX*MAX
+	mul $t0, $t0, 4 # t0 = MAX*MAX*4 (tamanho de bytes de cada matriz)
 	
 	li $v0, 9
-	move $a0, $t1
+	move $a0, $t0
 	syscall
 	move $s1, $v0 # s0 = endereco de A
 	
@@ -20,44 +20,45 @@
 	move $s2, $v0 # s1 = endereco de B
 
 	# loops do código
-	li $t2, 0 # t2 = i
+	li $t0, 0 # t0 = i = 0
 for_i:
-	bge $t2, $s0, for_i_end
-	li $t3, 0 # t3 = j
+	bge $t0, $s0, for_i_end
+	li $t1, 0 # t1 = j = 0
 for_j:
-	bge $t3, $s0, for_j_end
+	bge $t1, $s0, for_j_end
 
 	# calcular offset do elemento de A (max*i + j)*4
-	mul $t4, $t2, $s0  # t4 = max*i
-	add $t4, $t4, $t3  # t4 = max*i + j (offset no A)
-	mul $t4, $t4, 4  # t4 = 4*..
-	add $t4, $s1, $t4
+	mul $t2, $t0, $s0  # t2 = max*i
+	add $t2, $t2, $t1  # t2 = max*i + j (offset no A)
+	mul $t2, $t2, 4    # t2 = 4*(max*i + j)
+	add $t2, $s1, $t2
 	
 	# carregar elemento do A
-	l.s $f0, 0($t4)  # f0 = elemento do A
+	l.s $f0, 0($t2)  # f0 = elemento do A
 
 	# calcular offset do elemento de B (max*j + i)*4
-	mul $t5, $t3, $s0  # t5 = max*j
-	add $t5, $t5, $t2  # t5 = max*j + i
-	mul $t5, $t5, 4  # t5 = 4*...
-	add $t5, $s2, $t5
+	mul $t3, $t1, $s0  # t3 = max*j
+	add $t3, $t3, $t0  # t3 = max*j + i
+	mul $t3, $t3, 4    # t3 = 4*(max*j + i)
+	add $t3, $s2, $t3
 
 	# carregar elemento do B
-	l.s $f1, 0($t5)  # f1 = elemento do B
+	l.s $f1, 0($t3)  # f1 = elemento do B
 
 	# somar	elementos
 	add.s $f2, $f0, $f1
 
 	# salvar em A
-	s.s $f2, 0($t4)
+	s.s $f2, 0($t2)
 
-	addi $t3, $t3, 1
+	addi $t1, $t1, 1 # j += 1
 	j for_j
 for_j_end:
-	addi $t2, $t2, 1
+	addi $t0, $t0, 1 # i += 1
 	j for_i
 for_i_end:
 
+	# Saindo do programa
 	li $v0, 10
 	syscall
 	
--- Atividade 5/exe_2.asm
+++ Atividade 5/exe_2.asm
@@ -8,11 +8,11 @@
 	li $s1, BLOCK_SIZE
 
 	# alocar A e B
-	mul $t1, $s0, $s0 # t1 = MAX*MAX
-	mul $t1, $t1, 4 # t1 = MAX*MAX*4 (tamanho de bytes de cada matriz)
+	mul $t0, $s0, $s0 # t0 = MAX*MAX
+	mul $t0, $t0, 4 # t0 = MAX*MAX*4 (tamanho de bytes de cada matriz)
 	
 	li $v0, 9
-	move $a0, $t1
+	move $a0, $t0
 	syscall
 	move $s2, $v0 # s2 = endereco de A
 	
@@ -21,61 +21,59 @@
 	move $s3, $v0 # s3 = endereco de B
 
 	# loops do código
-	li $t2, 0 # t2 = i
+	li $t0, 0 # t0 = i = 0
 for_i:
-	bge $t2, $s0, for_i_end
-	li $t3, 0 # t3 = j
+	bge $t0, $s0, for_i_end
+	li $t1, 0 # t1 = j = 0
 for_j:
-	bge $t3, $s0, for_j_end
-	move $t9, $t2 # t9 = ii
+	bge $t1, $s0, for_j_end
+	move $t2, $t0 # t2 = ii = i
 for_ii:
-	add $t8, $t2, $s1 # t8 = i+block_size
-	bge $t9, $t8, for_ii_end
-	move $t8, $t3  # t8 = jj
+	add $t4, $t1, $s1 # t4 = i+block_size
+	bge $t2, $t4, for_ii_end
+	move $t3, $t1  # t3 = jj = j
 for_jj:
-	add $t7, $t3, $s1  # t7 = j+block_size
-	bge $t8, $t7, for_jj_end
-
-	#t2->t9
-	#t3->t8
+	add $t5, $t1, $s1  # t5 = j+block_size
+	bge $t5, $t3, for_jj_end
 
 	# calcular offset do elemento de A (max*ii + jj)*4
-	mul $t4, $t9, $s0  # t4 = max*ii
-	add $t4, $t4, $t8  # t4 = max*ii + jj (offset no A)
-	mul $t4, $t4, 4  # t4 = 4*..
-	add $t4, $s2, $t4
+	mul $t6, $t2, $s0  # t6 = max*ii
+	add $t6, $t6, $t3  # t6 = max*ii + jj (offset no A)
+	mul $t6, $t6, 4    # t6 = 4*(max*ii + jj)
+	add $t6, $s2, $t6  # t6 += endereco_a
 	
 	# carregar elemento do A
-	l.s $f0, 0($t4)  # f0 = elemento do A
+	l.s $f0, 0($t6)  # f0 = elemento do A
 
 	# calcular offset do elemento de B (max*j + i)*4
-	mul $t5, $t8, $s0  # t5 = max*jj
-	add $t5, $t5, $t9  # t5 = max*jj + ii
-	mul $t5, $t5, 4  # t5 = 4*...
-	add $t5, $s3, $t5
+	mul $t7, $t3, $s0  # t7 = max*jj
+	add $t7, $t7, $t2  # t7 = max*jj + ii
+	mul $t7, $t7, 4    # t7 = 4*(max*jj + ii)
+	add $t7, $s3, $t7  # t7 += endereco_b
 
 	# carregar elemento do B
-	l.s $f1, 0($t5)  # f1 = elemento do B
+	l.s $f1, 0($t7)  # f1 = elemento do B
 
 	# somar	elementos
 	add.s $f2, $f0, $f1
 
 	# salvar em A
-	s.s $f2, 0($t4)
+	s.s $f2, 0($t6)
 
 
-	addi $t8, $t8, 1
+	addi $t3, $t3, 1 # jj += 1
 	j for_jj
 for_jj_end:
-	addi $t9, $t9, 1
+	addi $t2, $t2, 1 # ii += 1
 	j for_ii
 for_ii_end:
-	add $t3, $t3, $s1
+	add $t1, $t1, $s1 # j += block_size
 	j for_j
 for_j_end:
-	add $t2, $t2, $s1
+	add $t0, $t0, $s1 # i += block_size
 	j for_i
 for_i_end:
 	
+	# Saindo do programa
 	li $v0, 10
 	syscall
