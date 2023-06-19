c9e428f25666b761575101fb37e07542da6cb189
Augusto <augusto.h.v.guerner@gmail.com>
2023-06-19 11:32:14 -0300

    Deixando melhor o uso dos regs

--- Atividade 5/exe_1.asm
+++ Atividade 5/exe_1.asm
@@ -3,44 +3,44 @@
 	
 	.eqv MATRIX_SIZE 4
 
-	li $s6, MATRIX_SIZE
+	li $s0, MATRIX_SIZE
 
 	# alocar A e B
 	
-	mul $t1, $s6, $s6 # t1 = MAX*MAX
+	mul $t1, $s0, $s0 # t1 = MAX*MAX
 	mul $t1, $t1, 4 # t1 = MAX*MAX*4 (tamanho de bytes de cada matriz)
 	
 	li $v0, 9
 	move $a0, $t1
 	syscall
-	move $s0, $v0 # s0 = endereco de A
+	move $s1, $v0 # s0 = endereco de A
 	
 	li $v0, 9
 	syscall
-	move $s1, $v0 # s1 = endereco de B
+	move $s2, $v0 # s1 = endereco de B
 
 	# loops do código
 	li $t2, 0 # t2 = i
 for_i:
-	bge $t2, $s6, for_i_end
+	bge $t2, $s0, for_i_end
 	li $t3, 0 # t3 = j
 for_j:
-	bge $t3, $s6, for_j_end
+	bge $t3, $s0, for_j_end
 
 	# calcular offset do elemento de A (max*i + j)*4
-	mul $t4, $t2, $s6  # t4 = max*i
+	mul $t4, $t2, $s0  # t4 = max*i
 	add $t4, $t4, $t3  # t4 = max*i + j (offset no A)
 	mul $t4, $t4, 4  # t4 = 4*..
-	add $t4, $s0, $t4
+	add $t4, $s1, $t4
 	
 	# carregar elemento do A
 	l.s $f0, 0($t4)  # f0 = elemento do A
 
 	# calcular offset do elemento de B (max*j + i)*4
-	mul $t5, $t3, $s6  # t5 = max*j
+	mul $t5, $t3, $s0  # t5 = max*j
 	add $t5, $t5, $t2  # t5 = max*j + i
 	mul $t5, $t5, 4  # t5 = 4*...
-	add $t5, $s1, $t5
+	add $t5, $s2, $t5
 
 	# carregar elemento do B
 	l.s $f1, 0($t5)  # f1 = elemento do B
--- Atividade 5/exe_2.asm
+++ Atividade 5/exe_2.asm
@@ -4,55 +4,55 @@
 	.eqv MATRIX_SIZE 4
 	.eqv BLOCK_SIZE 4
 
-	li $s6, MATRIX_SIZE
-	li $s7, BLOCK_SIZE
+	li $s0, MATRIX_SIZE
+	li $s1, BLOCK_SIZE
 
 	# alocar A e B
-	mul $t1, $s6, $s6 # t1 = MAX*MAX
+	mul $t1, $s0, $s0 # t1 = MAX*MAX
 	mul $t1, $t1, 4 # t1 = MAX*MAX*4 (tamanho de bytes de cada matriz)
 	
 	li $v0, 9
 	move $a0, $t1
 	syscall
-	move $s0, $v0 # s0 = endereco de A
+	move $s2, $v0 # s2 = endereco de A
 	
 	li $v0, 9
 	syscall
-	move $s1, $v0 # s1 = endereco de B
+	move $s3, $v0 # s3 = endereco de B
 
 	# loops do código
 	li $t2, 0 # t2 = i
 for_i:
-	bge $t2, $s6, for_i_end
+	bge $t2, $s0, for_i_end
 	li $t3, 0 # t3 = j
 for_j:
-	bge $t3, $s6, for_j_end
+	bge $t3, $s0, for_j_end
 	move $t9, $t2 # t9 = ii
 for_ii:
-	add $t8, $t2, $s7 # t8 = i+block_size
+	add $t8, $t2, $s1 # t8 = i+block_size
 	bge $t9, $t8, for_ii_end
 	move $t8, $t3  # t8 = jj
 for_jj:
-	add $t7, $t3, $s7  # t7 = j+block_size
+	add $t7, $t3, $s1  # t7 = j+block_size
 	bge $t8, $t7, for_jj_end
 
 	#t2->t9
 	#t3->t8
 
 	# calcular offset do elemento de A (max*ii + jj)*4
-	mul $t4, $t9, $s6  # t4 = max*ii
+	mul $t4, $t9, $s0  # t4 = max*ii
 	add $t4, $t4, $t8  # t4 = max*ii + jj (offset no A)
 	mul $t4, $t4, 4  # t4 = 4*..
-	add $t4, $s0, $t4
+	add $t4, $s2, $t4
 	
 	# carregar elemento do A
 	l.s $f0, 0($t4)  # f0 = elemento do A
 
 	# calcular offset do elemento de B (max*j + i)*4
-	mul $t5, $t8, $s6  # t5 = max*jj
+	mul $t5, $t8, $s0  # t5 = max*jj
 	add $t5, $t5, $t9  # t5 = max*jj + ii
 	mul $t5, $t5, 4  # t5 = 4*...
-	add $t5, $s1, $t5
+	add $t5, $s3, $t5
 
 	# carregar elemento do B
 	l.s $f1, 0($t5)  # f1 = elemento do B
@@ -70,10 +70,10 @@ for_jj_end:
 	addi $t9, $t9, 1
 	j for_ii
 for_ii_end:
-	add $t3, $t3, $s7
+	add $t3, $t3, $s1
 	j for_j
 for_j_end:
-	add $t2, $t2, $s7
+	add $t2, $t2, $s1
 	j for_i
 for_i_end:
 	
